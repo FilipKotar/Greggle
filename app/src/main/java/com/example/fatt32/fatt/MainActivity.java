02df20abcd4558c9a0aa89cad896fc1a458847ee
tandronescu <30327537+tandronescu@users.noreply.github.com>
2018-02-24 16:04:28 -0500

    Email Validation
    added email validation

--- app/src/main/java/com/example/fatt32/fatt/MainActivity.java
+++ app/src/main/java/com/example/fatt32/fatt/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.fatt32.fatt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/fatt32/fatt/SignUpPage.java
+++ app/src/main/java/com/example/fatt32/fatt/SignUpPage.java
@@ -2,6 +2,10 @@ package com.example.fatt32.fatt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpPage extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
+    public void submitAccount(View view) {
+        EditText edit_text   = (EditText)findViewById(R.id.email_field);
+
+        if (!isValidEmail(edit_text.getText())){
+            edit_text.setText("Invalid Email");
+        }
     }
 }

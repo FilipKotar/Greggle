753e532f8f45bcd809f9940c5d6e7d1c41515861
tandronescu <30327537+tandronescu@users.noreply.github.com>
2018-02-24 12:24:13 -0500

    New activity for sign up

--- app/src/main/java/com/example/fatt32/fatt/MainActivity.java
+++ app/src/main/java/com/example/fatt32/fatt/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.fatt32.fatt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void guestAction() {
 
+    }
+
+    public void accountCreation(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }

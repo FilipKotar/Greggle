be3c69618a45d23cf749ff111f92a06c83c34c1c
tandronescu <30327537+tandronescu@users.noreply.github.com>
2018-02-25 03:30:12 -0500

    database stuff

--- app/src/main/java/com/example/fatt32/fatt/MainActivity.java
+++ app/src/main/java/com/example/fatt32/fatt/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fatt32.fatt;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,6 +10,13 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -35,10 +44,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginAct(View view) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText password = (EditText) findViewById(R.id.password);
+        String emailStr = email.getText().toString();
+        String passStr = password.getText().toString();
+        final Context thisClass = this;
         // Do something in response to Login button.
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        firebaseAuth.signInWithEmailAndPassword(emailStr, passStr)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in passes? Display msg to user.
+                            Toast.makeText(thisClass, "Authentication Passed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // Go to School selection.
+                            Intent schoolAct = new Intent(thisClass, navigation.class);
+                            startActivity(schoolAct);
+                            // Kill off this activity.
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(thisClass, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 
